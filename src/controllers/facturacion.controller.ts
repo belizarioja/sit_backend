a977960bde897e9e00b5760de1ece3abdf263adc
Jesus Belizario <belizarioja@gmail.com>
2024-02-03 14:48:39 -0400

    corregir marca de agua de borrador y anulado

--- src/controllers/facturacion.controller.ts
+++ src/controllers/facturacion.controller.ts
@@ -590,22 +590,22 @@ export async function crearFactura (res: Response,_rif: any, _razonsocial: any, 
         const numeroafectado = (Number(_idtipodoc) === 2 || Number(_idtipodoc) === 3) ? '<br>' + tipoafectado + '<br>' + _numeroafectado + '<br>' + moment(_fechaafectado).format('DD/MM/YYYY hh:mm:ss a') : ''
         console.log("AMBIENTE")
         console.log(AMBIENTE)
-        console.log("Estatus")
-        console.log(_estatus)
-        const ANULADO = FILEPDF + 'utils/anulado.gif'
-        const BORRADOR = FILEPDF + 'utils/borrador.png'
-        const fondoanulado= `<img src="${ANULADO}" style="position: absolute; left:0; top:0; z-index:-10; width: 100%; height: 100%; "/>`
-        const fondoborrador = `<img src="${BORRADOR}" style="position: absolute; left:0; top:0; z-index:-10; width: 100%; height: 100%; "/>`
-        contenidoHtml = contenidoHtml.replace("{{fondomarca}}", '');
+        
         if(Number(_estatus) === 2) { // Si es anulado
-            console.log("AQUI 1")
-            console.log(ANULADO)
+            const ANULADO = FILEPDF + 'utils/anulado.gif'
+            const fondoanulado= `<img src="${ANULADO}" style="position: absolute; left:0; top:0; z-index:-10; width: 100%; height: 100%; "/>`
+            // console.log("AQUI 1")
+            // console.log(ANULADO)
             contenidoHtml = contenidoHtml.replace("{{fondomarca}}", fondoanulado);
         } else {
             if(AMBIENTE === 'local' || AMBIENTE === 'test') { // Si NO es Produccion
-                console.log("AQUI 2")
-                console.log(BORRADOR)
+                const BORRADOR = FILEPDF + 'utils/borrador.png'
+                const fondoborrador = `<img src="${BORRADOR}" style="position: absolute; left:0; top:0; z-index:-10; width: 100%; height: 100%; "/>`
+                // console.log("AQUI 2")
+                // console.log(BORRADOR)
                 contenidoHtml = contenidoHtml.replace("{{fondomarca}}", fondoborrador);
+            } else {
+                contenidoHtml = contenidoHtml.replace("{{fondomarca}}", '');
             }
         }
         contenidoHtml = contenidoHtml.replace("{{logo}}", IMGPDF+_rif + ".png");
